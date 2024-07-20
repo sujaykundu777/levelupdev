c9fa9239ac45edb2ed9c1cf86fbbafddc270f227
Sujay Kundu <sujaykundu777@gmail.com>
2024-07-20 18:49:38 +0530

    update get posts

--- levelupdev/src/app/admin/posts/page.tsx
+++ levelupdev/src/app/admin/posts/page.tsx
@@ -2,17 +2,38 @@ import { redirect } from "next/navigation";
 import { auth } from "@/auth";
 import { Button } from "flowbite-react";
 import Link from "next/link";
-import { getAllPosts } from "../../services";
+import { compareDesc } from "date-fns";
+import { getAllPosts } from "../../actions/octokit";
+import { allPosts } from "contentlayer/generated";
 
 const Admin = async () => {
   const session = await auth();
 
-  const allPosts = await getAllPosts();
-  console.log("postsFromGithub", allPosts);
+  const postsFromGithub = await getAllPosts();
+  console.log("postsFromGithub", postsFromGithub);
+
+  const postsFromContentlayer = allPosts;
+  console.log("postsFromContentlayer", postsFromContentlayer);
 
   if (!session) {
     redirect("/login");
   }
+
+  let posts = postsFromGithub
+    .map((githubPost: any) => {
+      let generatedPost = postsFromContentlayer.filter((generatedPost: any) => {
+        return generatedPost._raw.sourceFileName == githubPost.name;
+      })[0];
+
+      return {
+        ...githubPost,
+        date: generatedPost.publishedAt,
+        status: generatedPost ? "published" : "publising",
+      };
+    })
+    .sort((a: any, b: any) =>
+      compareDesc(new Date(a.publishedAt), new Date(b.publishedAt))
+    );
 
   return (
     <div className="pt-20 pb-5 text-center justify-center items-center container mx-auto">
@@ -21,6 +42,13 @@ const Admin = async () => {
       <Link href="/admin/posts/create">
         <Button> Create Post </Button>
       </Link>
+      {posts.map((post: any, idx: number) => (
+        <div key={idx} className="flex justify-center">
+          <h2 className="text-white">{post.name}</h2>
+          <Button> {post.status} </Button>
+          <Button> Edit </Button>
+        </div>
+      ))}
     </div>
   );
 };
